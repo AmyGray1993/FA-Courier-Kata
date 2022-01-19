f9a329125390c23ab034dac1f4c9a87320d458e7
AmyGray1993 <amy_gray1993@hotmail.co.uk>
2022-01-19 20:05:55 +0000

    Calculate parcel postage

--- FA-Courier-Kata/FA-Courier-Kata.Domain/Services/ParcelService.cs
+++ FA-Courier-Kata/FA-Courier-Kata.Domain/Services/ParcelService.cs
@@ -11,7 +11,26 @@ namespace FA_Courier_Kata.Domain.Services
 
         public decimal CalculateParcelPostage(Parcel parcel)
         {
-            return 0;
+            var width = parcel.Width;
+            var height = parcel.Height;
+            var depth = parcel.Depth;
+
+            if (width < 10 && height < 10 && depth < 10)
+            {
+                return 3;
+            }
+            else if (width < 50 && height < 50 && depth < 50)
+            {
+                return 8;
+            }
+            else if (width < 100 && height < 100 && depth < 100)
+            {
+                return 15;
+            }
+            else
+            {
+                return 25;
+            }
         }
     }
 }
