6a5175df0bf71ff3c030602f256d2150d1478375
AmyGray1993 <amy_gray1993@hotmail.co.uk>
2022-01-19 19:00:36 +0000

    Add initial tests for ParcelService

---
+++ FA-Courier-Kata/FA-Courier-Kata.Tests/ParcelServiceTests.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using FluentAssertions;
+using Xunit;
+
+namespace FA_Courier_Kata.Tests
+{
+    public class ParcelServiceTests
+    {
+        [Fact]
+        public void CalculateParcelPostage_AllDimensionsLessThan10_Return3()
+        {
+            // Arrange
+            var testItem = true;
+            // Act
+
+            // Assert
+            testItem.Should().BeTrue();
+        }
+
+        [Fact]
+        public void CalculateParcelPostage_OneDimensionIsGreaterThan10ButLessThan50_Return8()
+        {
+            // Arrange
+            var testItem = true;
+            // Act
+
+            // Assert
+            testItem.Should().BeTrue();
+        }
+
+        [Fact]
+        public void CalculateParcelPostage_OneDimensionIsGreaterThan50ButLessThan100_Return15()
+        {
+            // Arrange
+            var testItem = true;
+            // Act
+
+            // Assert
+            testItem.Should().BeTrue();
+        }
+
+        [Fact]
+        public void CalculateParcelPostage_OneDimensionIsGreaterThan100_Return25()
+        {
+            // Arrange
+            var testItem = true;
+            // Act
+
+            // Assert
+            testItem.Should().BeTrue();
+        }
+
+        [Fact]
+        public void CalculateParcelPostage_OneDimensionIsEqualTo100_Return25()
+        {
+            // Arrange
+            var testItem = true;
+            // Act
+
+            // Assert
+            testItem.Should().BeTrue();
+        }
+
+        [Fact]
+        public void CalculateParcelPostage_AllDimensionAreGreaterThan100_Return25()
+        {
+            // Arrange
+            var testItem = true;
+            // Act
+
+            // Assert
+            testItem.Should().BeTrue();
+        }
+    }
+}
